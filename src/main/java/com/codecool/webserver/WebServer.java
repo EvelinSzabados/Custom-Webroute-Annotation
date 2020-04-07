acab9439bc2d70ddc0303303265b33c2265437d4
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-07 18:39:52 +0200

    Create basic webserver for root

--- src/main/java/com/codecool/webserver/WebServer.java
+++ src/main/java/com/codecool/webserver/WebServer.java
@@ -1,4 +1,18 @@
 package com.codecool.webserver;
 
-public class Webserver {
+import com.codecool.webroute.RootController;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class WebServer {
+
+    void start() throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new RootController());
+        server.setExecutor(null);
+        server.start();
+    }
 }
