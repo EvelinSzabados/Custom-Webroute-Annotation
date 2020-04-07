d1eadc4709f1364bc9958a793374be296c60621b
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-07 18:40:15 +0200

    Display basic response on root

--- src/main/java/com/codecool/webroute/RootController.java
+++ src/main/java/com/codecool/webroute/RootController.java
@@ -1,4 +1,20 @@
 package com.codecool.webroute;
 
-public class RootController {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RootController implements HttpHandler {
+    public void handle(HttpExchange t) throws IOException {
+        InputStream is = t.getRequestBody();
+        is.read(); // .. read the request body
+        String response = "This is the response";
+        t.sendResponseHeaders(200, response.length());
+        OutputStream os = t.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 }
