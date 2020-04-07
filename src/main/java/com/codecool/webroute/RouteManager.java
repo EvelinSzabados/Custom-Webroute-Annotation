7ba12cfa3666563962f7cdcb839f3b270ab067d4
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-07 20:01:57 +0200

    Implement response to post request

--- src/main/java/com/codecool/webroute/RouteManager.java
+++ src/main/java/com/codecool/webroute/RouteManager.java
@@ -7,18 +7,30 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import static com.codecool.webroute.HttpMethod.GET;
+import static com.codecool.webroute.HttpMethod.POST;
+
 
 public class RouteManager {
 
     @WebRoute(method = GET, path = "/test")
     public void onTest(HttpExchange httpExchange) throws IOException {
-        InputStream is = httpExchange.getRequestBody();
-        is.read();
-        String response = "Test response";
+        String response = "<html><body><h1>Test response</h1><form action=\"/test2\" method=\"post\"><input type=\"submit\" value=\"Post request\"></input></form></body></html>";
+        displayResponse(response, httpExchange);
+
+    }
+
+    @WebRoute(method = POST, path = "/test2")
+    public void onTest2(HttpExchange httpExchange) throws IOException {
+        String response = "Response on test2 page";
+        displayResponse(response, httpExchange);
+
+    }
+
+    private void displayResponse(String response, HttpExchange httpExchange) throws IOException {
+
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
-        System.out.println("/test works");
     }
 }
